8922a39c66ad8c50f60f3747ffc9d54af905bd4c
Hanpeng Yang <38714377+AngeloHYang@users.noreply.github.com>
2021-12-27 14:41:20 +0800

    Update Web App
    - Working on data initialization

--- Web App Code/dataAccess.py
+++ Web App Code/dataAccess.py
@@ -2,23 +2,41 @@
     Functions related to data accessing, such as saving and reading predict models, necessary csv files
 '''
 
-def check_dataFiles():
-    return False
+import streamlit as st
+import os
 
+
+## Data File Related
+DataPath = "../Data/Crimes in Chicago_An extensive dataset of crimes in Chicago (2001-2017), by City of Chicago/"
+DataFileName = [
+    'Chicago_Crimes_2001_to_2004.csv', 
+    'Chicago_Crimes_2005_to_2007.csv', 
+    'Chicago_Crimes_2008_to_2011.csv', 
+    'Chicago_Crimes_2012_to_2017.csv'
+]
+
+def check_dataFiles():
+    FileExist = True
+    if os.path.exists(DataPath):
+        for i in DataFileName:
+            fullDataFileName = DataPath + i
+            if os.path.exists(fullDataFileName) == False:
+                FileExist = False
+                break
+    return FileExist
+
+
+## Comoonly used DataFrame Related
 def check_DataFrames():
     return False
 
+def create_DataFrames():
+    return check_DataFrames()
+
+
+## Prediction Model Related
 def check_models():
     return False
 
-def create_DataFrames():
-    if check_DataFrames():
-        return False
-    else:
-        pass
-
 def create_models():
-    if check_models():
-        return False
-    else:
-        pass
+    return check_models()
--- Web App Code/dataInitPage.py
+++ Web App Code/dataInitPage.py
@@ -8,10 +8,12 @@ import dataAccess
 import time
 
 def writeStatus(value):
+    statusEmpty = st.empty()
     if (value == True):
-        st.success("Here!")
+       statusEmpty.success("Here!")
     else:
-        st.error("Not detected!")
+        statusEmpty.error("Not detected!")
+    return statusEmpty
 
 def dataInitPage():
     st.session_state['dataInitDone'] = False # To make sure the system doesn't enter home page until data Init is done
@@ -20,10 +22,12 @@ def dataInitPage():
     modelStatus = dataAccess.check_models()
     
     if dfStatus == True and modelStatus == True:
+        print("Here!")
         st.session_state['dataInitDone'] = True # To make sure the system doesn't enter home page until data Init is done
     else:
         st.write("In order to run this project, you'll need to make sure these files exist:")
         
+        # Status Description
         name_col, status_col = st.columns(2)
         with name_col:
             st.write("")
@@ -32,12 +36,28 @@ def dataInitPage():
             st.write("")
             st.write("Prediction Models")
         with status_col:
-            writeStatus(dfStatus)
-            writeStatus(modelStatus)
+            dfStatusST = writeStatus(dfStatus)
+            modelStatusST = writeStatus(modelStatus)
         
         generateButton = st.button("Generate Now!")
         
+        # Button Activities
         if generateButton:
-            
-            st.experimental_rerun()
+            # If there's no data file, you'll be unable to create Commonly used DataFrames
+            if dataAccess.check_dataFiles():
+                if dfStatus == False:
+                    ## If there's no dataframes commonly used, we'll create them
+                    with dfStatusST:
+                        with st.spinner("Creating commonly used DataFrames..."):
+                            creationStatus = dataAccess.create_DataFrames()
+                        dfStatusST = writeStatus(creationStatus)
+                if modelStatus == False:
+                    with modelStatusST:
+                        with st.spinner("Creating Prediction Models"):
+                            creationStatus = dataAccess.create_models()
+                        modelStatusST = writeStatus(creationStatus)
+                        
+                st.experimental_rerun()
+            else:
+                st.error("We cannot find data file in ../Data/, so we can't create commonly used dataframes!")
             
